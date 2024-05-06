c9f00dafebbe0e0985b6dc3537d9efd7728167f0
UEEnthusiast <134012703+UEEnthusiast@users.noreply.github.com>
2024-05-06 14:41:54 +0200

    Add files via upload

---
+++ DroneMovementTimelineComponent.cpp
@@ -0,0 +1,139 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DroneMovementTimelineComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+UDroneMovementTimelineComponent::UDroneMovementTimelineComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void UDroneMovementTimelineComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetComponentTickEnabled(false);
+
+	if(!ensure(LocationCurve && HeightCurve))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LocationCurve && HeightCurve not defined"));
+		return;
+	}
+}
+
+
+// Called every frame
+void UDroneMovementTimelineComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                    FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	if (IsDroneMoving())
+	{
+		return;
+	}
+	
+	if(CheckIfNeedToStop())
+	{
+		Stop();
+	}
+	
+	const float CurrentTime = GetCurrentTime();
+	if(CheckIfFinished(CurrentTime))
+	{
+		Finished();
+	}
+	
+	// Calculating NewLocation
+	const FVector LerpLocation = FMath::Lerp(DroneStartLocation, DroneTargetLocation, GetCurrentLocationCurve(CurrentTime));
+	const FVector NewLocation = LerpLocation + FVector(0.f,0.f, GetCurrentHeightCurve(CurrentTime) * ZOffset);
+
+	// Calculating NewRotation
+	const FRotator NewRotation = FMath::RInterpTo(
+		DroneStaticMeshComponent->GetComponentRotation(), // Previous Rotation
+		UKismetMathLibrary::FindLookAtRotation(LerpLocation, GetTargetLocation()), // New Rotation
+		DeltaTime,
+		InterpSpeed);
+
+	// Updating Location & Rotation
+	DroneStaticMeshComponent->SetWorldLocationAndRotation(NewLocation, NewRotation, false, nullptr, ETeleportType::TeleportPhysics);
+}
+
+float UDroneMovementTimelineComponent::GetCurrentTime() const
+{
+	const float CurrentTime = UGameplayStatics::GetRealTimeSeconds(GetOwner()) - TimeWhenDroneStartedToMove;
+	
+	return bIsDroneReturning ? MaxDurationInSeconds - CurrentTime : CurrentTime;
+}
+
+bool UDroneMovementTimelineComponent::IsDroneMoving() const
+{
+	return TimeWhenDroneStartedToMove == -1.f;
+}
+
+FVector UDroneMovementTimelineComponent::GetTargetLocation() const
+{
+	return bIsDroneReturning ? DroneTargetLocation : DroneStartLocation;
+}
+
+float UDroneMovementTimelineComponent::GetCurrentLocationCurve(const float CurrentTime) const
+{
+	return LocationCurve->GetFloatValue(CurrentTime);
+}
+
+float UDroneMovementTimelineComponent::GetCurrentHeightCurve(const float CurrentTime) const
+{
+	return HeightCurve->GetFloatValue(CurrentTime);
+}
+
+void UDroneMovementTimelineComponent::MoveDrone(
+	const bool IsDroneReturningIn,
+	UStaticMeshComponent* DroneStaticMeshComponentIn,
+	AActor* SupplyStationIn,
+	const FVector& DroneStartLocationIn,
+	const FVector& DroneTargetLocationIn)
+{
+	bIsDroneReturning = IsDroneReturningIn;
+	DroneStaticMeshComponent = DroneStaticMeshComponentIn;
+	SupplyStation = SupplyStationIn;
+	DroneStartLocation = DroneStartLocationIn;
+	DroneTargetLocation = DroneTargetLocationIn;
+	
+	TimeWhenDroneStartedToMove = UGameplayStatics::GetRealTimeSeconds(GetOwner());
+	
+	SetComponentTickEnabled(true);
+}
+
+void UDroneMovementTimelineComponent::Stop()
+{
+	SetComponentTickEnabled(false);
+	
+	TimeWhenDroneStartedToMove = -1.f;
+	OnStopEvent.Broadcast();
+}
+
+void UDroneMovementTimelineComponent::Finished()
+{
+	SetComponentTickEnabled(false);
+	
+	TimeWhenDroneStartedToMove = -1.f;
+	OnFinishedEvent.Broadcast();
+}
+
+bool UDroneMovementTimelineComponent::CheckIfFinished(const float CurrentTime) const
+{
+	return bIsDroneReturning ?
+		(CurrentTime <= 0.f) :
+		(CurrentTime >= MaxDurationInSeconds);
+}
+
+bool UDroneMovementTimelineComponent::CheckIfNeedToStop() const
+{
+	return !SupplyStation || !DroneStaticMeshComponent;
+}
+
---
+++ DroneMovementTimelineComponent.h
@@ -0,0 +1,102 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Curves/CurveVector.h"
+#include "DroneMovementTimelineComponent.generated.h"
+
+// Declare a delegate type
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FSimpleEventDelegate);
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TESTPROJECT_API UDroneMovementTimelineComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UDroneMovementTimelineComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Curves")
+	UCurveFloat* LocationCurve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Curves")
+	UCurveFloat* HeightCurve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	float MaxDurationInSeconds = 30.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	float InterpSpeed = 5.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	float ZOffset = 1000.f;
+
+	UFUNCTION(BlueprintCallable, Category = "Events")
+	void MoveDrone(
+		const bool IsDroneReturning,
+		UStaticMeshComponent* DroneStaticMeshComponent,
+		AActor* SupplyStation,
+		const FVector& DroneStartLocation,
+		const FVector& DroneTargetLocation);
+
+	UFUNCTION(BlueprintCallable, Category = "Events")
+	void Stop();
+
+	UFUNCTION(BlueprintCallable, Category = "Events")
+	void Finished();
+
+	// Declare a Blueprint event using the delegate type
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FSimpleEventDelegate OnFinishedEvent;
+
+	// Declare a Blueprint event using the delegate type
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FSimpleEventDelegate OnStopEvent;
+
+private:
+
+	inline bool CheckIfFinished(const float CurrentTime) const;
+
+	inline bool CheckIfNeedToStop() const;
+
+	inline float GetCurrentTime() const;
+
+	inline bool IsDroneMoving() const;
+
+	inline FVector GetTargetLocation() const;
+	
+	inline float GetCurrentLocationCurve(const float CurrentTime) const;
+
+	inline float GetCurrentHeightCurve(const float CurrentTime) const;
+
+	UPROPERTY(VisibleAnywhere)
+	float TimeWhenDroneStartedToMove = -1.f;
+
+	UPROPERTY(VisibleAnywhere)
+	bool bIsDroneReturning = false;
+
+	UPROPERTY(EditAnywhere)
+	AActor* SupplyStation = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* DroneStaticMeshComponent = nullptr;
+	
+	UPROPERTY(EditAnywhere)
+	FVector DroneStartLocation = FVector::Zero();
+
+	UPROPERTY(EditAnywhere)
+	FVector DroneTargetLocation = FVector::Zero();
+};
